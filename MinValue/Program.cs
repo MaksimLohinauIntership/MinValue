63b9a226e163774b6ce0b21cf0a08201bccf30cf
Maksim Lohinau <Maksim.Lohinau@ventionteams.com>
2024-10-07 17:53:25 +0300

    Add second variant

--- MinValue/Program.cs
+++ MinValue/Program.cs
@@ -9,8 +9,12 @@ namespace MinValue
         static void Main(string[] args)
         {
             var list = new List<int>() { 66, 12, 25 };
+
             var min = GetMinimum(list);
             Console.WriteLine(min);
+
+            var minSecondVariant = GetMinimumSecondVariant(list);
+            Console.WriteLine(minSecondVariant);
         }
 
         public static int GetMinimum(List<int> list)
@@ -29,8 +33,38 @@ namespace MinValue
                     }
                 }
                 return min;
+            }   
+        }
+
+        public static int GetMinimumSecondVariant(List<int> list)
+        {
+            if (list == null || list.Count == 0)
+                throw new InvalidOperationException("Sequence contains no elements");
+
+            int num = 0;
+            bool flag = false;
+
+            foreach (int item in list)
+            {
+                if (flag)
+                {
+                    if (item < num)
+                    {
+                        num = item;
+                    }
+                }
+                else
+                {
+                    num = item;
+                    flag = true;
+                }
             }
-                
+
+            if (flag)
+            {
+                return num;
+            }
+            throw new NullReferenceException();
         }
     }
 }
